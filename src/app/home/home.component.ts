65ebe8fc9693bcb748a70978f374642fcd0a13a6
Mario Blokland <m.blokland@gambio.de>
2017-12-05 16:10:06 +0100

    Dynamically add new items

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  
+  itemCount: number;
+  btnText: string = 'Add Item';
+  goalText: string = 'My first life goal';
+  goals = [];
+  
+  constructor() {
   }
-
+  
+  ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+  
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
+  }
 }
